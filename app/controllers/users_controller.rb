b6f23c3fd8a78605455d8393b5d9f98b2d405dbd
Alex Grant <grantovich@csh.rit.edu>
2012-08-21 16:17:46 -0400

    Don't expose unsupported preferences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   
   def show
     render :json => {
-      :user => @current_user.as_json(:only => [:username, :real_name, :created_at, :updated_at, :preferences])
+      :user => @current_user.as_json(:only => [:username, :real_name, :created_at, :updated_at]).
+        merge({ :preferences => @current_user.preferences.slice(:thread_mode, :time_zone) })
     }
   end
   
