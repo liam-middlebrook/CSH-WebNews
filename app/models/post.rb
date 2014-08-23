a55e346dd1df18aa8fdb0b6552401ef1628dd3fc
Alex Grant <grantovich@csh.rit.edu>
2014-08-23 16:02:13 -0400

    Remove unneeded comment
    * The quirk no longer exists, but `to_i` is still necessary since
      the maximum personal_level could be NULL

--- app/models/post.rb
+++ app/models/post.rb
@@ -290,8 +290,6 @@ class Post < ActiveRecord::Base
   end
 
   def unread_personal_class_for_user(user)
-    # Quirk: Using `maximum` on a column from an implicit join table appears to
-    # sometimes return a string, hence `to_i`. Check this again under Rails 4.
     PERSONAL_CLASSES[user.unread_posts.where(thread_id: thread_id).maximum(:personal_level).to_i]
   end
 
