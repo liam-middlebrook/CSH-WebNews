142139c97907ae127281b7a820e15db13f26aa4d
Alex Grant <grantovich@csh.rit.edu>
2014-11-06 17:37:13 -0500

    Client controller can create/update users

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -2,7 +2,8 @@ class ClientController < ActionController::Base
   before_action :prevent_framing
 
   def show
-    if request.env[ENV_USERNAME].present?
+    if user_authenticated?
+      update_or_create_user!
       render layout: false
     else
       render 'shared/authenticate', layout: false
@@ -11,7 +12,21 @@ class ClientController < ActionController::Base
 
   private
 
+  def authenticated_username
+    request.env[ENV_USERNAME]
+  end
+
+  def update_or_create_user!
+    user = User.find_or_initialize_by(username: authenticated_username)
+    user.real_name = request.env[ENV_REALNAME]
+    user.save!
+  end
+
   def prevent_framing
     headers['X-Frame-Options'] = 'DENY'
+  end
+
+  def user_authenticated?
+    authenticated_username.present?
   end
 end
