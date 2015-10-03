6d5bee15bfedf4f4574fcb9b5dfd3c266fed2493
Alex Grant <grantovich@csh.rit.edu>
2015-10-03 11:40:56 -0400

    Update to Ember 2.1 and latest dependencies

--- frontend/app/serializers/user.js
+++ frontend/app/serializers/user.js
@@ -1,8 +1,6 @@
 import { ActiveModelSerializer } from 'active-model-adapter';
 
 export default ActiveModelSerializer.extend({
-  isNewSerializerAPI: true,
-
   normalize: function(modelClass, hash, property) {
     hash.id = 'current';
     return this._super(modelClass, hash, property);
